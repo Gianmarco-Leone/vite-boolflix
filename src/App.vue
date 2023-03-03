01d9d04d4279c7bb2efd5799c30c0c76e8cb8418
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-03-03 15:16:59 +0100

    add input tag + emit to transfer value

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
 <script>
-export default {};
+// Importo axios per fare chiamata API
+import axios from "axios";
+
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  data() {
+    return {
+      moviesEndpoint:
+        "https://api.themoviedb.org/3/search/movie?api_key=9e6d26513f128e25f1d7dea31384230a",
+    };
+  },
+  components: {
+    AppHeader,
+  },
+  methods: {
+    fetchFilms(url) {
+      axios.get(url).then((response) => {
+        console.log(response);
+      });
+    },
+
+    fetchFilteredMovies(term) {
+      this.fetchFilms(`${this.moviesEndpoint}&query=${term}`);
+    },
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <!-- HEADER -->
+  <AppHeader @on-search="fetchFilteredMovies" />
+</template>
 
 <style lang="scss"></style>
+
+<!-- `${this.moviesEndpoint}api_key=${this.api_key}&query=ritorno+al+futuro` -->
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      term: "",
+    };
+  },
+  emits: ["on-search"],
+  methods: {
+    search() {
+      this.$emit("on-search", this.term);
+    },
+  },
+};
+</script>
+
+<template>
+  <form action="">
+    <input
+      v-model="term"
+      type="text"
+      placeholder="Inserisci nome film/serie ..."
+    />
+    <button @click="search()" type="button">Cerca</button>
+  </form>
+</template>
+
+<style lang="scss" scoped></style>
