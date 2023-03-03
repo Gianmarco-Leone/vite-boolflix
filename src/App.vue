a9771e645bbb9dad4e849bb7bded1112d2af3e76
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-03-03 19:07:25 +0100

    add milestone 1

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,28 @@
 // Importo axios per fare chiamata API
 import axios from "axios";
 
+// -STATE- Importo file reattivo store.js
+import { store } from "./data/store.js";
+
 import AppHeader from "./components/AppHeader.vue";
+import MoviesList from "./components/MoviesList.vue";
+
 export default {
   data() {
     return {
+      store,
       moviesEndpoint:
-        "https://api.themoviedb.org/3/search/movie?api_key=9e6d26513f128e25f1d7dea31384230a",
+        "https://api.themoviedb.org/3/search/movie?api_key=9e6d26513f128e25f1d7dea31384230a&language=it-IT",
     };
   },
   components: {
     AppHeader,
+    MoviesList,
   },
   methods: {
     fetchFilms(url) {
       axios.get(url).then((response) => {
-        console.log(response);
+        store.movies = response.data.results;
       });
     },
 
@@ -30,8 +37,13 @@ export default {
 <template>
   <!-- HEADER -->
   <AppHeader @on-search="fetchFilteredMovies" />
+
+  <!-- MAIN -->
+  <MoviesList />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use "./assets/scss/style.scss";
+</style>
 
 <!-- `${this.moviesEndpoint}api_key=${this.api_key}&query=ritorno+al+futuro` -->
---
+++ src/components/MoviesList.vue
@@ -0,0 +1,38 @@
+<script>
+// -STATE- Importo file reattivo store.js
+import { store } from "../data/store.js";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <ul>
+    <li v-for="movie in store.movies">
+      <span> TITLE: {{ movie.title }} </span>
+      <br />
+      <span> ORIGINAL TITLE: {{ movie.original_title }} </span>
+      <br />
+      <span> LANGUAGE: {{ movie.original_language }} </span>
+      <br />
+      <span> VOTE: {{ movie.vote_average }} </span>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+// debug
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  li {
+    width: 300px;
+    border: 1px solid black;
+  }
+}
+</style>
